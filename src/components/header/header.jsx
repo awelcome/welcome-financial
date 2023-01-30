15433978c2b6dd394a663362f68b5b7b69196324
awelcome <github@welcome.simplelogin.com>
2023-01-30 15:12:09 -0700

    header created

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,36 @@
+import "./header.css";
+
 import React, { Component } from "react";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default class Header extends Component {
   render() {
     return (
       <>
-        <Container>
-          <p>Hello from Header</p>
+        <Container fluid>
+          <Row className="headerTitle">Welcome Financial Co.</Row>
+          <Row className="headerSubtitle">Welcome To Your Financial Future</Row>
+          <Row className="center" lg="auto">
+            <Button className="getStartedButton" variant="outline-dark">
+              <Container fluid>
+                <Row>
+                  <Col>
+                    <hr />
+                  </Col>
+                  <Col lg="auto" className="center">
+                    GET STARTED
+                  </Col>
+                  <Col>
+                    <hr />
+                  </Col>
+                </Row>
+              </Container>
+            </Button>
+          </Row>
         </Container>
       </>
     );
